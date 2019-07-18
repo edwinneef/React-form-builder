461f9d3b39b8cd00a19375b87d08349e0699473d
EdwinNeef <edwinneef@hoppinger.com>
2019-07-18 13:55:37 +0200

    .

--- src/components/OptionComponent.tsx
+++ src/components/OptionComponent.tsx
@@ -1,70 +1,82 @@
-import * as React from "react"
+import * as React from "react";
 import { Wizard, Step, Option } from "../types";
 
-export type OptionState = {
-}
+export type OptionState = {};
 export type OptionProps = {
-  data: Option
-  update:(new_step:Option) => void
-  valueIsSet?: boolean
-}
+  data: Option;
+  update: (new_step: Option) => void;
+  valueIsSet?: boolean;
+};
 
 export class OptionComponent extends React.Component<OptionProps, OptionState> {
   constructor(props: OptionProps, context) {
-    super(props)
-    this.state = {
-    }
+    super(props);
+    this.state = {};
   }
   render() {
-    
-    if(this.props.data.kind == "label"){
+    if (this.props.data.kind == "label") {
       return (
-      <div className="field">
-        <p>{this.props.data.title}</p>
-      </div>
-      )
-    }
-
-    if(this.props.data.kind == "input"){
-      let data = this.props.data
-      return (
-
-      <div className="field"> 
-        <label className="label" htmlFor={data.title}>{this.props.data.title}</label>
-        <div className="control">
-          <input className="input" 
-            id={ data.title }
-            onChange={ e => this.props.update({...data, value: ((data.hasValue && this.props.valueIsSet) || !data.hasValue) ? e.currentTarget.value : data.value }) }
-            placeholder={ data.hasValue && !this.props.valueIsSet ? `Please fill in or check the following field: ${ data.hasValue }` : '' }
-            value={ data.value }
-            type={ data.type ? data.type : 'text' } 
-          />
+        <div className="field">
+          <p>{this.props.data.title}</p>
         </div>
-      </div>
-
-      )
+      );
     }
 
-    if(this.props.data.kind == "checkbox"){
-      let data = this.props.data
+    if (this.props.data.kind == "input") {
+      let data = this.props.data;
       return (
-
-      <div onClick={e => this.props.update({...data, selected: !data.selected})}>
-        <label className="checkbox" htmlFor={data.title}>
-          <input type="checkbox" checked={data.selected} id={data.title} />          
-          {this.props.data.title}
-        </label>
-      </div>
-
-      )
+        <div className="field">
+          <label className="label" htmlFor={data.title}>
+            {this.props.data.title}
+          </label>
+          <div className="control">
+            <input
+              className="input"
+              id={data.title}
+              onChange={e =>
+                this.props.update({
+                  ...data,
+                  value:
+                    (data.hasValue && this.props.valueIsSet) || !data.hasValue
+                      ? e.currentTarget.value
+                      : data.value
+                })
+              }
+              placeholder={
+                data.hasValue && !this.props.valueIsSet
+                  ? `Please fill in or check the following field: ${
+                      data.hasValue
+                    }`
+                  : ""
+              }
+              value={data.value}
+              type={data.type ? data.type : "text"}
+            />
+          </div>
+        </div>
+      );
     }
-    
-    return (
 
-    <div>
-      <p>Sorry, unknown step</p>
-    </div>
-    
-    )
+    if (this.props.data.kind == "checkbox") {
+      let data = this.props.data;
+      return (
+        <div
+          onClick={e =>
+            this.props.update({ ...data, selected: !data.selected })
+          }
+        >
+          <label className="checkbox" htmlFor={data.title}>
+            <input type="checkbox" checked={data.selected} id={data.title} />
+            {this.props.data.title}
+          </label>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <p>Sorry, unknown step</p>
+      </div>
+    );
   }
 }
